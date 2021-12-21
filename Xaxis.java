14f622a94f348b51927089453ba06c75f5bff0ee
arunvinu <arunkumarxworkz@gmail.com>
2021-12-21 18:07:13 +0545

    view

---
+++ Xaxis.java
@@ -0,0 +1,7 @@
+class Xaxis{
+public static void main (String [ ]a){
+int [ ] xgraph = { 0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+System.out.println(xgraph [ 0 ]);
+System.out.println(xgraph [4 ]);
+}
+}
